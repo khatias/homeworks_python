84c274bca1fff588e5a7408f31ae3e8094eea3a1
khatias <sikharuldizekhatiaa@gmail.com>
2024-03-23 15:38:21 -0400

    done

--- HW_26/exercises26.py
+++ HW_26/exercises26.py
@@ -1,5 +1,6 @@
+
 import unittest
-from vehicle import Vehicle
+from vehicle import Vehicle, ElectricVehicle
 
 class TestVehicle(unittest.TestCase):
     def setUp(self):
@@ -10,6 +11,16 @@ class TestVehicle(unittest.TestCase):
 
     def test_drive(self):
         self.assertEqual(self.vehicle.drive, "The Camry is now driving.")
+
+class TestElectricVehicle(unittest.TestCase):
+    def setUp(self):
+        self.electric_vehicle = ElectricVehicle("Tesla", "Model S", 2022)
+
+    def test_charge(self):
+        self.assertEqual(self.electric_vehicle.charge, "The vehicle is now charged.")
+
+    def test_fuel_up(self):
+        self.assertEqual(self.electric_vehicle.fuel_up, "This vehicle has no fuel tank!")
 
 if __name__ == "__main__":
     unittest.main()
