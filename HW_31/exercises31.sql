a97bfda12f9ef38aec2ab31cd87a9442c4718a30
khatias <sikharuldizekhatiaa@gmail.com>
2024-04-08 23:00:14 -0400

    done

---
+++ HW_31/exercises31.sql
@@ -0,0 +1,38 @@
+-- select * from customers;
+-- 1. დაწერეთ SQL ბრძანება, რომლის საშუალებითაც customers ცხრილიდან წამოიღებთ მხოლოდ customerName, phone, city, country ველებს.
+
+select customerName, phone, city, country customerName from customers
+limit 10;
+
+
+
+--  დაწერეთ SQL ბრძანება, რომლის საშუალებითაც customers ცხრილიდან წამოიღებთ ყველა იმ ჩანაწერს რომლის ფოსტის კოდი მეტია 1370ზე ან salesRepEmployeeNumber მეტია 150
+select * from customers 
+where postalCode > 1370 or salesRepEmployeeNumber > 150
+limit 10;
+
+
+-- 3. დაწერეთ SQL ბრძანება, რომლის საშუალებითაც customers ცხრილიდან წამოიღებთ ისეთ ჩანაწერს, რომელშიც customerName შეიცავს 'Mini' ტექსტს
+select * from customers 
+where customerName like '%mini%'
+limit 10;
+
+
+-- 4. დაწერეთ SQL ბრძანება, რომლის საშუალებითაც customers ცხრილიდან წამოიღებთ მონაცემებს, რომელსაც აქვს state 'CA' ან 'NY'
+select * from customers
+ where state  in ("NY", "CA")
+ limit 10;
+
+
+-- 5. დაწერეთ SQL ბრძანება, რომლის საშუალებითაც customers ცხრილიდან წამოიღებთ მონცემებს, რომელსაც აქვს creditLimit 10000-ზე მეტი 
+select * from customers
+where creditLimit > 10000
+limit 10;
+
+
+
+
+
+
+
+
